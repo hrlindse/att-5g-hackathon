aacc07aaad68e8494a71e2dfd68dadb94d7212a4
Holly Lindsey <lindsey.holly@yahoo.com>
2021-10-24 08:22:58 -0400

    Add styling and props ref

--- components/buttons/ActionButton.js
+++ components/buttons/ActionButton.js
@@ -2,21 +2,32 @@ import { useState, useEffect } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import { Text, View } from "react-native";
+import theme from "../../assets/styles/theme.style.js";
 
-export default function ActionButton() {
+export default function ActionButton(props) {
   return (
-    <View style={styles.container}>
-      <Text>Action Button (full width)</Text>
+    <View style={styles.actionButton}>
+      <Text style={styles.text}>{props.text}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: 50,
-    height: 50,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
+  actionButton: {
+    flex: 1,
+    fontFamily: theme.FONT_FAMILY_BUTTON,
+    color: theme.FONT_COLOR_BUTTON,
+    backgroundColor: theme.PRIMARY_COLOR,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    marginTop: 10,
+    borderRadius: 10,
+    textAlign: "center",
+    padding: 15,
+  },
+  text: {
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
