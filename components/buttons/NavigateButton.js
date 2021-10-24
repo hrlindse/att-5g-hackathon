4680f9bf14e3428ed8f0e9172a94f8156ac50ae3
Holly Lindsey <lindsey.holly@yahoo.com>
2021-10-24 08:24:29 -0400

    Testing styles - delete later
    This component is no longer referenced - using ActionButton

--- components/buttons/NavigateButton.js
+++ components/buttons/NavigateButton.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import theme from "../../assets/styles/theme.style.js";
 
 export default function NavigateButton() {
   return (
     <View style={styles.container}>
-      <Text>Nav</Text>
+      <Text style={styles.text}>Get Directions</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: 50,
-    height: 50,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
+    flex: 1,
+    fontFamily: theme.FONT_FAMILY_BUTTON,
+    color: theme.FONT_COLOR_BUTTON,
+
+    backgroundColor: theme.PRIMARY_COLOR,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    marginTop: 10,
+    borderRadius: 10,
+    textAlign: "center",
+    padding: 15,
+  },
+  text: {
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
