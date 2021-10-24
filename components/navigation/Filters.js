7329287d6df292170c4cdd3d1d1b2ec32b45bdd1
Holly Lindsey <lindsey.holly@yahoo.com>
2021-10-24 08:25:36 -0400

    Create Filters.js

---
+++ components/navigation/Filters.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { Text, View } from "react-native";
+import theme from "../../assets/styles/theme.style.js";
+import { commonStyles } from "../../assets/styles/styles";
+import ActionButton from "../buttons/ActionButton";
+import Filter from "./Filter";
+
+export default function Filters() {
+  return (
+    <View style={styles.container}>
+      <View style={commonStyles.topBarContainer}>
+        <View style={commonStyles.topBar}>
+          <View></View>
+          <Text style={commonStyles.heading}>Filter Locations</Text>
+          <Image
+            style={styles.close}
+            source={require("../../assets/images/close.png")}
+          ></Image>
+        </View>
+      </View>
+      <View style={styles.filters}>
+        <Filter text="Locate friends"></Filter>
+        <Filter text="Exit doors"></Filter>
+        <Filter text="Food courts"></Filter>
+        <Filter text="Restrooms"></Filter>
+        <Filter text="Events in the stadium"></Filter>
+      </View>
+
+      <View style={styles.bottom}>
+        <ActionButton text="Apply Filter"></ActionButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    fontFamily: theme.FONT_FAMILY_BUTTON,
+    color: theme.FONT_COLOR_BUTTON,
+    fontSize: 16,
+    backgroundColor: theme.LT_GREY_COLOR,
+  },
+  close: {
+    width: 35.25,
+    height: 28.5,
+  },
+  filters: {
+    marginTop: 50,
+  },
+  bottom: {
+    width: "100vw",
+    position: "fixed",
+    bottom: 0,
+  },
+});
