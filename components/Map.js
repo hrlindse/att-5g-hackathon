011949978355cd43b44c70dff10c48b5a4c1d691
Holly Lindsey <lindsey.holly@yahoo.com>
2021-10-24 08:27:37 -0400

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,26 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import HamburgerButton from "./buttons/HamburgerButton";
-import FilterButton from "./buttons/FilterButton";
-import NavigateButton from "./buttons/NavigateButton";
-import SOSButton from "./buttons/SOSButton";
+
 import {
   SmartMapView,
   SmartMapManager,
 } from "react-native-steerpath-smart-map";
 import { SMARTMAP_API_KEY } from "@env";
+import theme from "../assets/styles/theme.style.js";
 
 export default function Map() {
   //   SmartMapManager.start(SMARTMAP_API_KEY);
   return (
     <View style={styles.container}>
       <Text>Map test page!</Text>
-      <StatusBar style="auto" />
-      <HamburgerButton></HamburgerButton>
-      <SOSButton></SOSButton>
-      <FilterButton></FilterButton>
-      <NavigateButton></NavigateButton>
+      {/* <StatusBar style="auto" /> */}
 
       {/* <SmartMapView style={{ flex: 1 }} apiKey={SMARTMAP_API_KEY} /> */}
     </View>
@@ -29,9 +23,8 @@ export default function Map() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    backgroundColor: theme.LT_GREY_COLOR,
   },
 });
