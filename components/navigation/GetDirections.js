12a6b4f5be133c8c4d030465c56062c762a4ba6a
Holly Lindsey <lindsey.holly@yahoo.com>
2021-10-24 08:26:06 -0400

    Style source/destination fields

--- components/navigation/GetDirections.js
+++ components/navigation/GetDirections.js
@@ -1,15 +1,26 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity, Text, View, Image } from "react-native";
 import React from "react";
-import { Text, View } from "react-native";
+import theme from "../../assets/styles/theme.style.js";
 
 export default function GetDirections() {
   return (
     <View style={styles.container}>
-      <Text>Get Directions component</Text>
-      {/* TODO x to close */}
-      {/* TODO start point */}
-      {/* TODO destination */}
+      <Text style={styles.label}>Get Directions:</Text>
+      <View style={styles.textbox}>
+        <Text style={styles.source}>Your location</Text>
+        <Image
+          style={styles.mic}
+          source={require("../../assets/images/microphone.png")}
+        ></Image>
+      </View>
+      <View style={styles.textbox}>
+        <Text style={styles.destination}>Enter seat number</Text>
+        <Image
+          style={styles.mic}
+          source={require("../../assets/images/microphone.png")}
+        ></Image>
+      </View>
       {/* TODO get directions button */}
     </View>
   );
@@ -17,10 +28,29 @@ export default function GetDirections() {
 
 const styles = StyleSheet.create({
   container: {
-    width: 50,
-    height: 50,
+    paddingHorizontal: 20,
     backgroundColor: "#fff",
+    shadowOffset: 4,
+    shadowColor: theme.FONT_COLOR,
     // alignItems: "center",
     // justifyContent: "center",
   },
+  label: {
+    fontWeight: "bold",
+    paddingVertical: 8,
+  },
+  textbox: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    fontWeight: "bold",
+    backgroundColor: theme.LT_GREY_COLOR,
+    margin: 4,
+    borderRadius: 6,
+  },
+  source: { fontWeight: "bold" },
+  destination: {},
+  mic: { width: 14, height: 18 },
 });
