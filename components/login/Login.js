9a6408a21bb34a3e14f138f07cf754aeb9589d49
Holly Lindsey <lindsey.holly@yahoo.com>
2021-10-24 13:04:00 -0400

    Update Login.js

--- components/login/Login.js
+++ components/login/Login.js
@@ -1,39 +1,39 @@
-import { useState, useEffect } from "react";
-import { StyleSheet, TouchableOpacity, Text, View, Image } from "react-native";
-import React from "react";
-import theme from "../../assets/styles/theme.style.js";
-import { commonStyles } from "../../assets/styles/styles";
+import React, { useRef, useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 import ActionButton from "../buttons/ActionButton";
 import LoginEntry from "./LoginEntry";
 import Checkbox from "expo-checkbox";
-import CarouselComp from "react-native-snap-carousel";
+import Carousel, { Pagination } from "react-native-snap-carousel";
 import CarouselItem, { sliderWidth, itemWidth } from "./CarouselItem";
 
+//styles
+import theme from "../../assets/styles/theme.style.js";
+import { commonStyles } from "../../assets/styles/styles";
+
 export default function Login() {
-  onboardSlides = {
-    activeIndex: 0,
-    carouselItems: [
-      {
-        uri: "../../assets/images/back.png",
-        text: "Text 1",
-      },
-      {
-        uri: "../../assets/images/back.png",
-        text: "Text 2",
-      },
-      {
-        uri: "../../assets/images/back.png",
-        text: "Text 3",
-      },
-    ],
-  };
-  _renderItem = ({ item, index }) => {
-    return (
-      <View style={styles.slide}>
-        <Text style={styles.title}>{item.title}</Text>
-      </View>
-    );
-  };
+  const [carousel, setCarousel] = useState();
+  const carouselItems = [
+    {
+      uri: require("../../assets/images/login-1.png"),
+      text: "Locate your loved ones at any given point",
+    },
+    {
+      uri: require("../../assets/images/login-2.png"),
+      text: "Navigate to your seat seamlessly",
+    },
+    {
+      uri: require("../../assets/images/login-3.png"),
+      text: "Stay updated with all the offers in the stadium",
+    },
+  ];
+
+  //   const isCarousel = useRef(null);
+  const [activeSlide, setActiveSlide] = useState();
+
+  function _renderItem({ item, index }) {
+    return <CarouselItem data={item} />;
+  }
+
   return (
     <View style={styles.container}>
       <View style={commonStyles.topBarContainer}>
@@ -41,20 +41,47 @@ export default function Login() {
           <Image
             style={styles.backButton}
             source={require("../../assets/images/back.png")}
-          ></Image>
+          />
           <Text style={commonStyles.heading}>Login</Text>
           <View></View>
         </View>
       </View>
-      <CarouselComp
-        layout="tinder"
-        layoutCardOffset={9}
-        ref={isCarousel}
-        data={carouselItems}
-        renderItem={CarouselItem}
-        sliderWidth={sliderWidth}
-        itemWidth={itemWidth}
-      />
+      <View style={styles.carousel}>
+        <Carousel
+          ref={(c) => {
+            setCarousel(c);
+          }}
+          data={carouselItems}
+          renderItem={_renderItem}
+          sliderWidth={sliderWidth}
+          itemWidth={itemWidth}
+          onSnapToItem={(index) => setActiveSlide(index)}
+        />
+        {/* <CarouselComp
+          style={styles.carouselComp}
+          layout="default"
+          ref={isCarousel}
+          data={carouselItems}
+          renderItem={CarouselItem}
+          sliderWidth={sliderWidth}
+          itemWidth={itemWidth}
+        /> */}
+        <Pagination
+          carouselRef={carousel}
+          tappableDots="true"
+          dotsLength={carouselItems.length}
+          activeDotIndex={activeSlide}
+          dotStyle={{
+            width: 15,
+            height: 15,
+            borderRadius: 10,
+            marginHorizontal: -5,
+            backgroundColor: theme.SECONDARY_COLOR,
+          }}
+          inactiveDotOpacity={0.5}
+        />
+      </View>
+
       <View style={styles.bottom}>
         <LoginEntry></LoginEntry>
         <ActionButton text="Submit"></ActionButton>
@@ -77,6 +104,18 @@ const styles = StyleSheet.create({
   backButton: {
     width: 21,
     height: 31,
+  },
+  carousel: {
+    // marginTop: 50,
+    height: 250,
+    borderTopWidth: 3,
+    borderTopColor: theme.LT_GREY_COLOR,
+    backgroundColor: "#fff",
+    // width: "100vw",
+  },
+  carouselComp: {
+    // height: 300,
+    // paddingTop: 80,
   },
   bottom: {
     width: "100vw",
