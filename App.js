4ed1195fea91d36368ec093be5bfda53fdbc1651
Holly Lindsey <lindsey.holly@yahoo.com>
2021-10-24 13:04:09 -0400

    Add routing

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import { FIREBASE_API_KEY, FIREBASE_SENDER_ID, FIREBASE_APP_ID } from "@env";
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
 // import { Barometer } from "expo-sensors";
 // import Barometer from "react-native-barometer";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Map from "./components/Map";
 import Home from "./components/Home";
 import Filters from "./components/navigation/Filters";
@@ -51,6 +60,8 @@ onAuthStateChanged(auth, (user) => {
   }
 });
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   // const isSupported = Barometer.isSupported();
 
@@ -77,13 +88,22 @@ export default function App() {
   // }
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Login></Login>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Menu" component={Menu} />
+          <Stack.Screen name="Filters" component={Filters} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SOS" component={SOS} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: "#000",
+  },
 });
