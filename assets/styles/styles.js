c4ea0717bb7f4264ec00729eed87df8b80d87d09
Holly Lindsey <lindsey.holly@yahoo.com>
2021-10-24 08:22:16 -0400

    Create common styles file

---
+++ assets/styles/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+import theme from "../styles/theme.style.js";
+
+// Common styles across the application
+export const commonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    flex: 1,
+    width: "fit-content",
+    textAlign: "center",
+    fontFamily: theme.FONT_FAMILY_HEAD,
+    color: theme.FONT_COLOR,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  topBarContainer: { position: "fixed", width: "100vw" },
+  topBar: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    backgroundColor: "#FFFF",
+    zIndex: 10,
+  },
+});
