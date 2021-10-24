b1f1fc3e7eeb92c04f3c3be35b4afa93da6cd369
Holly Lindsey <lindsey.holly@yahoo.com>
2021-10-24 08:26:22 -0400

    Style and content

--- components/SOS.js
+++ components/SOS.js
@@ -1,21 +1,54 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, TouchableOpacity, Text, View, Image } from "react-native";
+import React from "react";
+import theme from "../assets/styles/theme.style.js";
+import { commonStyles } from "../assets/styles/styles";
+import ActionButton from "./buttons/ActionButton";
+import MenuItem from "./menu/MenuItem";
 
 export default function SOS() {
   return (
     <View style={styles.container}>
-      <Text>SOS test page!</Text>
-      <StatusBar style="auto" />
+      <View style={commonStyles.topBarContainer}>
+        <View style={commonStyles.topBar}>
+          <View></View>
+          <Text style={commonStyles.heading}>Emergency Services</Text>
+          <Image
+            style={styles.close}
+            source={require("../assets/images/close.png")}
+          ></Image>
+        </View>
+      </View>
+      <View style={styles.items}>
+        <MenuItem
+          uri={require("../assets/images/phone.png")}
+          text="Person Missing"
+        ></MenuItem>
+        <MenuItem
+          uri={require("../assets/images/phone.png")}
+          text="Contact Security"
+        ></MenuItem>
+        <MenuItem
+          uri={require("../assets/images/phone.png")}
+          text="Medical Emergency"
+        ></MenuItem>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    fontFamily: theme.FONT_FAMILY_BUTTON,
+    color: theme.FONT_COLOR_BUTTON,
+    fontSize: 16,
+    backgroundColor: theme.LT_GREY_COLOR,
+  },
+  close: {
+    width: 35.25,
+    height: 28.5,
+  },
+  items: {
+    marginTop: 50,
   },
 });
